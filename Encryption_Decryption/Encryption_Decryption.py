e4adb325aca9fe947d75a94a2dcb787b02e9a5c6
TSPT19901 <ahbor337@gmail.com>
2025-12-13 17:13:29 +0700

    added comment

--- Encryption_Decryption/Encryption_Decryption.py
+++ Encryption_Decryption/Encryption_Decryption.py
@@ -3,21 +3,22 @@ import os
 import ast
 from Tool_Option import tool_option
 
+# create a base Key class
 class Key:
     def __init__(self, public_key=None, private_key=None):
         self.__public_key = public_key
         self.__private_key = private_key
-
+#create the functon to get public/private key from init function
     def get_public_key(self):
         return self.__public_key
 
     def get_private_key(self):
         return self.__private_key
-
+#function to generate key(both public and private)
     def new_key(self):
         self.__public_key, self.__private_key = rsa.newkeys(2048)
         return self.__public_key, self.__private_key
-
+#function for save both key to file.pem 
     def save_public_key(self, filename="Encryption_Decryption/public.pem"):
         with open(filename, "wb") as f:
             f.write(self.__public_key.save_pkcs1("PEM"))
@@ -25,14 +26,14 @@ class Key:
     def save_private_key(self, filename="Encryption_Decryption/private.pem"):
         with open(filename, "wb") as f:
             f.write(self.__private_key.save_pkcs1("PEM"))
-
+#function to load key from file.pem
     def load_keys(self, pub_file="Encryption_Decryption/public.pem", priv_file="Encryption_Decryption/private.pem"):
         with open(pub_file, "rb") as f:
             self.__public_key = rsa.PublicKey.load_pkcs1(f.read())
         with open(priv_file, "rb") as f:
             self.__private_key = rsa.PrivateKey.load_pkcs1(f.read())
 
-
+#create class for 2 methods encrypton and decryption
 class EncryptionDecryption(Key):
     def encryption(self, msg):
         encrypted = rsa.encrypt(msg.encode(), self.get_public_key())
@@ -41,12 +42,15 @@ class EncryptionDecryption(Key):
     def decryption(self, encrypted):
         decrypted = rsa.decrypt(encrypted, self.get_private_key())
         return decrypted.decode()
-
+#function to display and execute all methods here
 def display_and_execute():
+#style loading
     tool_option.loading_display()
     tool_option.clear_after(0.1)
-    re_file =0
+    re_file =0 #variable for track if user start encrypt again or not 
+    #create object using EncryptionDecryption class
     obj = EncryptionDecryption()
+    #condition here to check is key file is existed or not
     if not os.path.exists("Encryption_Decryption/public.pem") or not os.path.exists("Encryption_Decryption/private.pem"):
         obj.new_key()
         obj.save_public_key()
@@ -59,12 +63,16 @@ def display_and_execute():
     while True:
         print("\nPlease choose an option:\n1. Encrypt text\n2. Decrypt text\n3. Exit")
         option = input("Option: ")
+        #for option 1, when user encrypt the new text, file will override
         if option == "1":
             if re_file==0:
                 text = input("Enter the text to encrypt: ")
                 encrypted = obj.encryption(text)
+                #we use encoding="utf-8" to tell python how to covert data from byte to string for save to file 
                 with open("Encryption_Decryption/HistoryText.txt", "w", encoding="utf-8") as f:
-                    f.write(repr(encrypted) + "\n")
+                    #we use repr function to convert from byte to strign here 
+                    #The file will be use to decrypt for entire text in the file 
+                    f.write(repr(encrypted) + "\n") 
                 re_file+=1
             elif re_file>0:
                 text = input("Enter the text to encrypt: ")
@@ -75,14 +83,18 @@ def display_and_execute():
 
         elif option == "2":
             file_name= input("Please input your file name: ")
+            #to check if encrypted file is exist or not 
             if not os.path.exists(file_name):
                 print("No history found.")
                 continue
+            #then if file is existed, open that file and start reading 
             with open(file_name, "r", encoding="utf-8") as f:
                 lines = f.readlines()
             print("\nDecrypted messages:")
             for line in lines:
-                encrypted_bytes = ast.literal_eval(line.strip())
+                #we make new variable and use ast.literal_eval to convert back from strign to byte
+                encrypted_bytes = ast.literal_eval(line.strip())  # use .strip() to eliminate or skip the new line
+                #after convert, we can call our function decryption
                 decrypted_text = obj.decryption(encrypted_bytes)
                 print(decrypted_text)
 
