def8de6c04bf8a717d3fa72884ca3f84de1008f0
Sunlong422 <tanghounsunlong@gmail.com>
2025-12-13 14:02:43 +0700

    tra

--- Signup_Login/login_signup.py
+++ Signup_Login/login_signup.py
@@ -1,5 +1,6 @@
 from Tool_Option import tool_option
 from Password_Strength_Analyzer import Password_Strength_Analyzer
+#using getpass to make blink when input password
 from getpass import getpass
 
 dict_store_account = {} #store username password to file : account.txt
@@ -26,7 +27,7 @@ def save_data_to_File(dict, filename, key, value):#save data from dictionary to 
     with open(filename, "a") as file:
         file.write(f"{key} = {value}\n")
 
-
+#display option for users to create/login account
 def sign_up_login():
     while True:
         
@@ -47,29 +48,29 @@ def sign_up_login():
             print("Invalid!!!")
             print("Input Only [S], [L], [E]\n")
         elif input_choice == "L":
-            login()
-            
+            login()   
             continue
         elif input_choice == "S":
             sign_up()
             continue
         elif input_choice == "E":
             break
-
+#load data from file for check. compare
 dict_store_account = load_data("DATA/account.txt")
-
+#function that use for create account
 def sign_up(): #Sign up account
-    
+    #Get user input to create their account
     while True:
         username = input("Username: ")
 
-        if len(username) > 20:
+        if len(username) > 20:#check ensure password not too long
             print("Your username too long!\n\n\n")
             continue
-        if username in dict_store_account:
+        if username in dict_store_account:#check if password is already exists
             print("Username already exists!!\n\n\n")
             continue
         break
+    #Try to check strength password each user account ensure it strong 
     while True:
         password = input("Password: ")  
         has_number= False
@@ -89,26 +90,26 @@ def sign_up(): #Sign up account
                     has_upper=True
                 if(character in numbers):  #number check
                     has_number=True
-                if(character in special):    #special check
+                if(character in special): #special check
                     has_special=True
                 if (character.islower()):
-                    has_lower=True        #lower check
+                    has_lower=True       #lower check
         if (has_number and has_special and has_upper and has_lower):
             print("Sign up seccessfully!!!\n\n")
             save_data_to_File(dict_store_account, "DATA/account.txt", username, password)
             Password_Strength_Analyzer.history_account(username)
+            #create automatic history for each user after account is created
             part = "History_each_user/" + username + ".txt"
-
             with open(part, "w"):
                 pass
             break
-
+#using encapsulation to compare password
 class authentication:
     def __init__(self, username, password):
         self.username = username
         self.__password = password
-
-    def compare_private_password(self, password_attempt): #login account   
+    
+    def compare_private_password(self, password_attempt): #compare password to login account   
         if password_attempt == self.__password:
             print("Login successfully <3\n\n")
 
@@ -124,8 +125,7 @@ def login(): #login account
         if username in dict_store_account:
             user1 = authentication(username , dict_store_account[username]) #object and pass value to attribute of class
             password = getpass("Password: ")
-            user1.compare_private_password(password)
-            
+            user1.compare_private_password(password)      
         else:
             print("Username does not exist!!!\n\n") 
 
